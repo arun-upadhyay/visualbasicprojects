23f6cd3c0165c455e075eb0380c98dada347997e
Arun Kumar Upadhyay <Arun Kumar Upadhyay>
2016-12-24 12:01:57 -0600

    committed

---
+++ LibraryManagementSystem/LibraryManagementSystem/frmRegisterSoftware.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRegisterSoftware
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmRegisterSoftware))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Button1.Location = New System.Drawing.Point(12, 35)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(144, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Continue Unregistered"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button2.Location = New System.Drawing.Point(172, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Register"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Counter Limit"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Maroon
+        Me.Label2.Location = New System.Drawing.Point(171, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Counter Value"
+        '
+        'frmRegisterSoftware
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(273, 65)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmRegisterSoftware"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Activate LMS"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ LibraryManagementSystem/LibraryManagementSystem/frmRegisterSoftware.vb
@@ -0,0 +1,78 @@
+﻿Imports Machine
+Imports Machine.MachineInfo
+Public Class frmRegisterSoftware
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        My.Settings.Trail = My.Settings.Trail - 1
+        My.Settings.Save()
+        Label2.Text = My.Settings.Trail
+        If My.Settings.Trail >= 0 Then
+            frmSplash.Show()
+            Me.Close()
+        End If
+        If My.Settings.Trail < 0 Then
+            MsgBox("You Trail Software is Expired, Please Purcharse Product Key", MsgBoxStyle.Information)
+            Dim enterKey = InputBox("Please Enter Product Key")
+            If enterKey = Trim(generateUniqueKey()) Then
+                My.Settings.TrailE = "Yes"
+                My.Settings.Save()
+                frmSplash.Show()
+                Me.Close()
+            Else
+                Me.Close()
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim enterKey = InputBox("Please Enter Product Key")
+        If enterKey = Trim(generateUniqueKey()) Then
+            My.Settings.TrailE = "Yes"
+            My.Settings.Save()
+            frmSplash.Show()
+            Me.Close()
+        Else
+            MsgBox("Invalid Product Key", MsgBoxStyle.Critical, "LMS")
+
+        End If
+    End Sub
+
+    Private Sub frmRegisterSoftware_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.Fixed3D
+        Label2.Text = My.Settings.Trail
+        If My.Settings.TrailE = "Yes" Then
+            frmSplash.Show()
+            Me.Close()
+        End If
+    End Sub
+    Function generateUniqueKey()
+        Dim SerialKey As String = ""
+        Dim MyMachine As MachineInfo = New MachineInfo
+        'Creating machine Object
+        Dim Myprcessors() As ProcessorIfo
+        'Fill Processors Information Object
+        Myprcessors = MyMachine.Processors
+        'Assighning the first Processor to its TextBox
+        SerialKey = Mid(Myprcessors(0).ProcessorId, 1, 5) + "-"
+
+        Dim MyHardDisks() As HardDiskIfo
+        'Fill HardDisk Information Object
+        MyHardDisks = MyMachine.HardDisks
+        'Assighning the first Hard Disk Model to its TextBox
+        SerialKey = SerialKey + Mid(MyHardDisks(0).model, 1, 5) + "-"
+        'Dimension Components  information for Computer Id .  Contains eight components
+        Dim MyId As MachineidComponentsInfo
+        'Fill Machine id component information object
+        MyId = MyMachine.MachineIdComponents
+        'Assigning  the eight Machine id component  to corresponding TextBox
+        SerialKey = SerialKey + MyId.Component2 + "-"
+        SerialKey = SerialKey + MyId.Component1 + "-"
+        Dim MyActivation As MachineActivationComponentsInfo
+        'Fill Activation code component information object. it takes four strings parameters To Have Variety for activation Code For Different versions
+        MyActivation = MyMachine.MachineActivationComponents("Arun", "Sotware", "LIBRARY MANAGEMENT", "SOFTWARE")
+        'Assigning  the eight Activation Code component  to corresponding TextBox
+        SerialKey = SerialKey + MyActivation.Component1 + "-"
+        SerialKey = SerialKey + MyActivation.Component2
+        Return (SerialKey)
+    End Function
+End Class
